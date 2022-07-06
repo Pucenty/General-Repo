e8cd301d94680d485a7871d3d713ba895a59e7da
Pucenty <mrpucenty@gmail.com>
2022-07-06 14:11:56 +0200

    DEVOOOOOOOOPS POWAHHHH

--- windows_shedule_shutdown.bat
+++ windows_shedule_shutdown.bat
@@ -4,11 +4,14 @@ echo DO YOU WANT YOUR COMPUTER TO SHUTDOWN? (y/n)
 set /p Input=Enter y or n:
 if /I "%Input%"=="y" (goto yes) else (goto no)
 :yes
+shutdown -a
 echo In how many minutes do you want to commence shutdown?
 set /p input= Type any input:
-echo Input is: %input%
-set /A sekundy=%input%*60
-echo sekundy is: %sekundy%
-shutdown /s /t %sekundy%
+set /A input_in_seconds=%input%*60
+
+echo Shutting down system in %input_in_seconds% seconds (%input% minutes).
+shutdown /s /t %input_in_seconds%
 :no
-echo a to zdecyduj sie kurwa
+echo POZDRO
+pause
+cls
