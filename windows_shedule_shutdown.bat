9527bbb6d978e68543f5045003aa04cd60d14a93
Pucenty <mrpucenty@gmail.com>
2022-07-06 03:29:31 +0200

    Windows shutdown script

---
+++ windows_shedule_shutdown.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo DO YOU WANT YOUR COMPUTER TO SHUTDOWN? (y/n)
+set /p Input=Enter y or n:
+if /I "%Input%"=="y" goto yes
+goto no
+
+:yes
+echo In how many minutes do you want to commence shutdown?
+set /p input= Type any input:
+echo Input is: %input%
+set /A sekundy=%input%*60
+echo sekundy is: %sekundy%
+shutdown /s /t %sekundy%
+:no
+echo a to zdecyduj sie kurwa
