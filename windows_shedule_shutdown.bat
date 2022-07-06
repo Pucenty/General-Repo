3851c237d41fe489e210d315837f0c559dc5f7c8
Pucenty <mrpucenty@gmail.com>
2022-07-06 03:53:54 +0200

    provide if else logic

--- windows_shedule_shutdown.bat
+++ windows_shedule_shutdown.bat
@@ -2,9 +2,7 @@
 
 echo DO YOU WANT YOUR COMPUTER TO SHUTDOWN? (y/n)
 set /p Input=Enter y or n:
-if /I "%Input%"=="y" goto yes
-goto no
-
+if /I "%Input%"=="y" (goto yes) else (goto no)
 :yes
 echo In how many minutes do you want to commence shutdown?
 set /p input= Type any input:
